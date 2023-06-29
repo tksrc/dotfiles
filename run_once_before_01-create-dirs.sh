f218a87d64fe18f3e7e650d24f7af6fc97c3669a
Teodor Kostadinov <teo@privatemail.us>
2023-06-29 16:00:27 +0300

    updates

---
+++ run_once_before_01-create-dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # exit immediately if any command returns a non-zero exit code
+set -f # disable filename expansion (globbing)
+
+printf "\n🚀 Creating directories\n"
+
+mkdir -p ~/Code
+mkdir -p ~/Temp
+mkdir -p ~/Cust
+mkdir -p ~/.go
+
+printf "\n✅ Complete\n"
--- run_once_before_create-dirs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "\n🚀 Creating directories\n"
-
-mkdir -p ~/Code/repos
-mkdir -p ~/Temp
-mkdir -p ~/Customers
-mkdir -p ~/.go
